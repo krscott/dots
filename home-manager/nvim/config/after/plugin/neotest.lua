bab2a9f4e82230708ec63c8f907313caf44ffc8e
Kris Scott <kscott91@gmail.com>
2024-11-07 11:09:38 -0800

    nvim: Add neotest keymaps

--- home-manager/nvim/config/after/plugin/neotest.lua
+++ home-manager/nvim/config/after/plugin/neotest.lua
@@ -1,7 +1,35 @@
-require("neotest").setup({
+local neotest = require("neotest")
+
+neotest.setup({
     adapters = {
         require("neotest-gtest").setup({
             debug_adapter = "gdb",
         }),
     },
 })
+
+local wk_exists, wk = pcall(require, 'which-key')
+if (wk_exists) then
+    wk.add({
+        { "<leader>dt", group = "neotest" },
+    })
+end
+
+vim.keymap.set(
+    "n",
+    "<leader>dtn",
+    neotest.run.run,
+    { desc = "neotest: Run nearest test" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>dto",
+    neotest.summary.open,
+    { desc = "neotest: Open Summary" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>dtc",
+    "<cmd>ConfigureGtest<cr>",
+    { desc = "neotest-gtest: Configure" }
+)
