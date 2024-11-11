404d67d0c3652fe2c5743c1960484078c274a552
Kris Scott <kscott91@gmail.com>
2024-11-10 16:58:22 -0800

    nvim: Add some debug code

--- home-manager/nvim/config/lua/krisvim/init.lua
+++ home-manager/nvim/config/lua/krisvim/init.lua
@@ -1,3 +1,16 @@
 require("krisvim.remap")
 require("krisvim.options")
 require("krisvim.jumplist")
+
+function dumps(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. dumps(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
--- home-manager/nvim/config/lua/krisvim/jumplist.lua
+++ home-manager/nvim/config/lua/krisvim/jumplist.lua
@@ -1,8 +1,26 @@
+-- function print_jumplist()
+--     local current_win = vim.api.nvim_get_current_win()
+--     local jumplist, current_idx = unpack(vim.fn.getjumplist(current_win))
+
+--     for i, jump in pairs(jumplist) do
+--         local bufname = vim.api.nvim_buf_get_name(jump.bufnr)
+--         print(i - current_idx - 1, bufname, dumps(jump))
+--     end
+-- end
+
+local function is_valid_jump(bufname)
+    if string.find(bufname, "neo%-tree filesystem") then
+        return false
+    end
+
+    return true
+end
+
 local function jump_safe(jumplist, i, current_idx)
     local jump = jumplist[i]
     if jump then
         local bufname = vim.api.nvim_buf_get_name(jump.bufnr)
-        if not string.find(bufname, "neo%-tree filesystem") then
+        if is_valid_jump(bufname) then
             local cmd
             if i > current_idx then
                 cmd = tostring(i - current_idx) .. '<C-i>'
