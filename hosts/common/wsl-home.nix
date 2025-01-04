fffb783fbab6c2fd7cdf038b54ff60038b46f78b
Kris Scott <kscott91@gmail.com>
2025-01-04 15:46:21 -0600

    Disable pay-respects in wsl

--- hosts/common/wsl-home.nix
+++ hosts/common/wsl-home.nix
@@ -1,7 +1,10 @@
-{...}: {
+{lib, ...}: {
   krs = {
     git.useSystemSsh = true;
     secrets.enable = true;
     wsl.enable = true;
   };
+
+  # TODO(kscott): Debug
+  programs.pay-respects.enable = lib.mkForce false;
 }
